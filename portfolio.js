e6b0c04538de1d847cd2bd70663fa22994cc294b
Kaivalya Nandurkar <kaivalya.nandurkar2@gmail.com>
2022-02-05 00:16:24 +0530

    heading updated

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
 
-const textArray = ["DEVELOPER BY PASSION", "DEVELOPER BY CHOICE"];
+const textArray = ["A WEB DEVELOPER", "A WEB DESIGNER"];
 const typingDelay = 100;
 const erasingDelay = 50;
 const newTextDelay = 1000;
